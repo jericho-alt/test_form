29a6f14989aeb32ad8569159520e4c6990f96e91
Jericho <78802739+jericho-alt@users.noreply.github.com>
2024-06-07 08:16:56 -0400

    Update index.php

--- index.php
+++ index.php
@@ -12,20 +12,20 @@
 
 	<form method="POST">
 		<input type="text" name="first_name" placeholder="First name" value="">
-		<span class = "error"><?php echo $first_nameErr ?>
+		<span class = "error">
 	</span><br>
 		<input type="text" name="middle_name" placeholder="Middle Name" value="">
-		<span class = "error"><?php echo $middle_nameErr ?>
+		<span class = "error">
 	</span><br>
 		<input type="text" name="last_name" placeholder="Last name" value="">
-		<span class = "error"><?php echo $last_nameErr ?>
+		<span class = "error">
 	</span><br>
 
 		<select name="gender"  >
 			<option name = "gender"></option>
-			<option name = "gender" <?php if($gender == "male"){echo "selected"; } ?> value = "">Male</option>
-			<option name = "gender" <?php if($gender == "female"){echo "selected"; } ?> value = "">Female</option>
-		</select><span class = "error"><?php echo $genderErr ?></span><br>
+			<option name = "gender" value = "">Male</option>
+			<option name = "gender" value = "">Female</option>
+		</select><span class = "error"></span><br>
 
 		<input type="text" name = "email" placeholder="Email" value = ""><span class = "error"><?php echo $emailErr ?>
 	</span><br>
